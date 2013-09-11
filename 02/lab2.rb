caddaa9268b5bf86c6e7e3314ca6cf570d99f72b
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2013-09-11 12:06:19 -0600

    CSCI 3308 (Software Engineering Methods and Tools) lab work.

---
+++ 02/lab2.rb
@@ -0,0 +1,13 @@
+
+
+# Part1: Hello World
+class HelloWorldClass
+    def initialize(name)
+        @name = name.capitalize
+    end
+    def sayHi
+        puts "Hello #{@name}!"
+    end
+end
+hello = HelloWorldClass.new("Michael")
+hello.sayHi
